9754a533a6fb3bc2d07d4247c4b234b7507797c7
AlbertGStanica <59702001+AlbertGStanica@users.noreply.github.com>
2020-01-09 13:37:44 -0400

    Add files via upload
    commit

--- Browser.java
+++ Browser.java
@@ -1,2 +1,48 @@
+import java.util.Scanner;
+
+
 public class Browser {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        int input;
+        BookmarkList bookmark_list = new BookmarkList();
+
+        while (running) {
+            printMenu();
+            //printsTheMenu
+
+            input = sc.nextInt();
+
+            if (input == 0){
+                running = false;
+            }
+            else{
+
+                if(input == 1){
+                    // Add Bookmark
+                    Bookmark new_bookmark = new Bookmark(sc.nextLine(), sc.nextLine());
+                    bookmark_list.addBookmark(new_bookmark);
+                }
+
+                if(input == 2){
+                    //Printout Bookmark
+                    bookmark_list.printList();
+                }
+
+            }
+
+        }
+
+    }
+     public static void printMenu(){
+        System.out.println("Menu");
+        System.out.println("-------");
+        System.out.println("Enter 0 To Stop");
+        System.out.println("1 - Add Bookmark");
+        System.out.println("2 - Print list of Bookmarks");
+
+    }
 }
