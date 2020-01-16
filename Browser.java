ae29dab17487ca12df19f33496c0f4cad08b207b
EthanG78 <EthanGarnier78@gmail.com>
2020-01-16 16:13:45 -0400

    Cleaned up some code

--- Browser.java
+++ Browser.java
@@ -10,55 +10,60 @@ public class Browser {
 
         boolean running = true;
         int input;
+        String ID, url, address, command;
 
         Scanner sc = new Scanner(System.in);
 
+        //Creates runtime instance to execute commands
         Runtime runtime = Runtime.getRuntime();
 
         BookmarkList bookmark_list = new BookmarkList();
 
-        while (running) {
-            printMenu();
+        while (running)
+        {
             //printsTheMenu
+            printMenu();
 
             input = sc.nextInt();
 
-            if (input == 0){
+            if (input == 0)
+            {
                 running = false;
             }
-            else{
-
-                if(input == 1){
+            else {
+                if(input == 1)
+                {
                     // Add Bookmark
                     System.out.print("ID for the bookmark: ");
-                    String ID = sc.next();
+                    ID = sc.next();
 
                     System.out.print("Web address or file name: ");
-                    String url = sc.next();
+                    url = sc.next();
 
                     Bookmark new_bookmark = new Bookmark(ID, url);
                     bookmark_list.addBookmark(new_bookmark);
                 }
 
-                if(input == 2){
+                if(input == 2)
+                {
                     //Printout Bookmark
                     System.out.println("Bookmark list: ");
                     bookmark_list.printList();
                 }
 
                 if(input == 3){
-                    // Launces the Website
+                    // Launches the Website
                     System.out.print("Which bookmark do you want to display? ");
-                    String webAddress, ID, command;
 
                     int bookMarkIndex = sc.nextInt();
+
                     if(bookmark_list.getBookmark(bookMarkIndex) == null)
                     {
                         System.out.println("That bookmark does not exist");
                     }
                     else
                     {
-                        webAddress = bookmark_list.getBookmark(bookMarkIndex).getWebAddress();
+                        address = bookmark_list.getBookmark(bookMarkIndex).getWebAddress();
                         ID = bookmark_list.getBookmark(bookMarkIndex).getID();
 
 
@@ -66,15 +71,14 @@ public class Browser {
 
                         try
                         {
-                            if (webAddress.endsWith(".pdf"))
+                            if (address.endsWith(".pdf"))
                             {
-                                command = "C:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\AcroRd32 " + webAddress;
+                                command = "C:\\Program Files (x86)\\Adobe\\Acrobat Reader DC\\Reader\\AcroRd32 " + address;
                                 runtime.exec(command);
                             }
                             else
                             {
-                                command = "C:\\Program Files (x86)\\Internet Explorer\\iexplore " + webAddress;
-                                //command = "firefox " + webAddress;
+                                command = "C:\\Program Files (x86)\\Internet Explorer\\iexplore " + address;
                                 runtime.exec(command);
                             }
                         }
