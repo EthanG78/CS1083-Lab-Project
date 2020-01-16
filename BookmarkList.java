5355b55902bd0421c829c6dd40e713bf8081c705
EthanG78 <ethangarnier78@gmail.com>
2020-01-16 13:17:12 -0400

    Fixed getBookmark of index that doesn't exist

--- BookmarkList.java
+++ BookmarkList.java
@@ -39,7 +39,12 @@ public class BookmarkList
     }
 
     public Bookmark getBookmark(int index){
-        return list[index];
+        if(index < nBookmarks)
+        {
+            return list[index];
+        }
+
+        return null;
     }
 
     public void printList()
